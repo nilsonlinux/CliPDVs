c6332565ef4453e70f2a8a9b505f372cd042c113
Nilsonlinux <nilsonlinux@gmail.com>
2020-06-18 00:11:02 -0300

    Add files via upload

--- AtualizadorCliPDVs.sh
+++ AtualizadorCliPDVs.sh
@@ -26,6 +26,41 @@ version='20200408'
       \____|_|_|_|   |____/  \_/ |___/ © 2020
               ${c}Versão :${enda} ${y}${version}  ${enda}"
 ###########################################################
+#!/bin/bash
+#
+# CliPDVs.sh
+# Automatização sobre demanda
+# Nilsonlinux 15/06/2020
+# Colabore com o projeto
+#
+#Veriaveis
+IPSERV=8.8.8.8
+a='\033[1;33m'                 # Amarelo
+end='\E[0m'
+echo "====================================================="
+echo "Aguarde enquanto testamos se o ip de destino encontra-se acessível"
+if ! ping -c 5 $IPSERV >> /dev/null ; then
+echo "=========================="
+echo " Você não está conectado. "
+echo "=========================="
+exit
+else
+echo "========================="
+echo " Conectado !!! "
+echo "========================="
+fi
+echo "====================================================="
+################## PROGRESSO STATUS ##############################
+BAR='Conexão bem sucedida ...'   # MÁXIMO 20 CARACTERES
+sleep 1
+for i in {1..20}; do
+    echo -ne "\r${BAR:0:$i}"
+    sleep .1
+done
+sleep 1
+echo
+echo "====================================================="
+echo -e "$t Conectado... IP -$a Atualizando CliPDVs $end.";
 echo -e "${y}Atualizando (CliPDVs). Por favor aguarde . . .${enda}"
 sleep 3
 echo -e "${y}(CliPDVs) está sendo atualizado e será executado logo após . . .${enda}"
