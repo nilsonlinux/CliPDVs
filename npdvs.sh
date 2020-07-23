90302afbabcd7f7a77b3e9cd1aa890af09438200
Nilsonlinux <nilsonlinux@gmail.com>
2020-07-23 10:04:49 -0300

    Update npdvs.sh

--- npdvs.sh
+++ npdvs.sh
@@ -11,7 +11,7 @@ i#!/bin/bash
 # endc=end-color end=end-argument
 pdvs_ips='139 131 122 123 124 25 102 103 104 105 107 120 140 133 110 11 112 113 114 130 116 55 59 117 225 132 138 128' #FINAL dos IPS DOS PDVS...
 gt="100"
-version="2.7"
+version="2.8"
 GMCORE='6.36'
 IPSERV='192.168'
 RES="1920x1030"
@@ -135,7 +135,7 @@ cliupdate () {
   echo && echo -en " ${y}Precione ENTER para continuar${endc}"
   read input
   echo && echo -e " Atualizando ${b}CliPDVs${end}, Por favor aguarde..."
-  wget https://raw.githubusercontent.com/nilsonlinux/CliPDVs/master/new_script.sh -O $spath/npdvs.sh &>/dev/null
+  wget https://raw.githubusercontent.com/nilsonlinux/CliPDVs/master/npdvs.sh -O $spath/npdvs.sh &>/dev/null
   sleep 1 && echo -e " ${b}CliPDVs${end} Atualização aplicada com sucesso "
   sleep 1 && echo -e " Restartando ${b}CliPDVs${end}..."
   sleep 2
